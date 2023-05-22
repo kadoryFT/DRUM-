2f9b6ae0889552153056e3e0f3a031fba534d454
kadoryFT <72969287+kadoryFT@users.noreply.github.com>
2023-05-22 09:25:39 +0300

    done

--- index.js
+++ index.js
@@ -5,7 +5,23 @@ for (var i = 0; i<numberOfButten; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
         var buttenInnerHtml =this.innerHTML
-        switch (buttenInnerHtml) {
+        makesound(buttenInnerHtml)
+        buttenanimeted(buttenInnerHtml)
+        }
+
+        
+    );
+}
+
+    
+    addEventListener("keydown", function(event){
+            
+            makesound(event.key)
+            buttenanimeted(event.key)
+        });
+    
+    function makesound(key){
+        switch (key) {
             case "w":
                 var tom1 = new Audio("sounds/tom-1.mp3")
                 tom1.play();
@@ -38,16 +54,18 @@ for (var i = 0; i<numberOfButten; i++) {
         
             default:
                 break;
-        }
 
-        
-    });
+    }};
 
-    
-        
-    
+function buttenanimeted(currentkey){
+    var activebutten =document.querySelector("." + currentkey);
+    activebutten.classList.add("pressed");
 
+setTimeout(function(){
+    activebutten.classList.remove("pressed");
+}, 100);
 
 }
+
 
 
